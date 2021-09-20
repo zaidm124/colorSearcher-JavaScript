bf03087bb9dc23996d14bcfa26f36a26900bb72e
zaidm124 <zaidm124@gmail.com>
2021-09-20 13:30:06 +0530

    console commented

--- script/new.js
+++ script/new.js
@@ -1,6 +1,6 @@
 function inpValue() {
   var value = document.querySelector("input").value;
-  console.log(value);
+  // console.log(value);
   var allColours = ["red", "blue", "green", "yellow"];
   var final = [];
   for (let i = 0; i < allColours.length; i++) {
@@ -9,18 +9,18 @@ function inpValue() {
     }
   }
   var length = document.getElementById("colors").children.length;
-  console.log(length);
+  // console.log(length);
   for (let i = 0; i < length; i++) {
     var color = document.getElementById("colors").children[i];
     color.classList.add("disp");
   }
-  console.log(final);
+  // console.log(final);
   for (let i = 0; i < final.length; i++) {
     var show = final[i];
     for (let j = 0; j < length; j++) {
       var color = document.getElementById("colors").children[j];
       if (show === color.id) {
-        console.log(color);
+        // console.log(color);
         color.classList.remove("disp");
       }
     }
